436677e0448cc602cd6b259a6ae9f57847b415f4
SparXFusion <s2234nadar@gmail.com>
2022-01-29 13:18:40 +0530

    CPH1859: add initial makefiles
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

---
+++ device.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/oppo/CPH1859/CPH1859-vendor.mk)
